0e9932586dbd3bbbadb29cf641361c7014699cc7
EnzoMinoru <134522847+EnzoMinoru@users.noreply.github.com>
2023-11-10 12:32:22 -0300

    WW

--- script.js
+++ script.js
@@ -1,3 +1,2 @@
 function mudarTexto() {
-    document.getElementById("demo").innerHTML = "Texto alterado!";
-}
+    document.getElementById("demo").innerHTML = "22 campeonatos paulistas, 3 libertadores, 8 campeonatos brasileiros, 1 copa do brasil... "}
